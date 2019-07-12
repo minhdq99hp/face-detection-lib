946d9b5403e3eaa4cb92ee191dddbedfddcad69f
minhdq99hp <minhdq99hp@gmail.com>
2019-07-12 14:09:51 +0700

    update

--- example/cnn_dlib.py
+++ example/cnn_dlib.py
@@ -2,22 +2,19 @@ import cv2
 import sys, os
 sys.path.append(os.path.realpath('..'))
 
-from lib.models import HaarCascade
+from lib.models import CNNDlib
 
 img = cv2.imread('assets/example-03.jpg')
 
-faceDetector = HaarCascade()
+faceDetector = CNNDlib()
 
-# faces = faceDetector.detect(img)
 faces = faceDetector.detect_with_evaluation(img)
 
 for (x1, y1, x2, y2) in faces:
-    cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+    cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-show = cv2.resize(img, (1280, 720))
-cv2.imshow('Haar-cascade', show)
+cv2.imshow('CNN based Dlib', img)
 
 # cv2.imwrite('output/example-03.jpg', img)
-
 cv2.waitKey(0)
 cv2.destroyAllWindows()
